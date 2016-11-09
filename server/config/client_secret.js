246ce2b1c33ca3f2be2d4b973e5caf639526fa11
surbhi <surbhi.poswalia@gmail.com>
2016-11-09 22:40:51 +0000

    Implement google OAuth with minor changes in routes

---
+++ server/config/client_secret.js
@@ -0,0 +1,17 @@
+ var IDs = {
+     google:{
+        "client_id": "1052230810202-ma6okp671o29233hsh0o1skcdc7ql8rf.apps.googleusercontent.com",
+        "project_id": "parabolic-rhino-141800",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "SGwhqeabJJIv6hzQ7SCqCjO2",
+        "callbackURL": "https://space-repetion-app-surbi.c9users.io/auth/google/callback"
+     }
+}
+module.exports = IDs;
+
+
+
+
+
