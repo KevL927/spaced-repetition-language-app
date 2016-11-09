85a416f4e2c36ce9a119c5d02b676469123e1314
surbhi <surbhi.poswalia@gmail.com>
2016-11-09 00:55:22 +0000

    add get request for First Question display and update post request for next question response

--- server/index.js
+++ server/index.js
@@ -44,34 +44,13 @@ var runServer = function(callback) {
 if (require.main === module) {
     runServer();
 }
-
-// app.get('/question', function(req, res){
-//     var questionId = '5820fb638ba76026b9bf8b8d';
-//     Questions.findOne({_id:questionId}, function(err, question){
-//         if (err) return errorHandler(res);
-//         return res.json(question);
-//     });
-// });
-
-app.get('/question', function(req, res){
-    var questionId = '5820fb638ba76026b9bf8b8d';
-    Questions.find({}, function(err, question){
-        if (err) return errorHandler(res);
-        return res.json(question);
-    });
-});
-app.get('/getUsers', function(req, res){
-    User.find({}, function(err, users){
-        if (err) return errorHandler(res);
-        return res.json(users);
-    });
-});
+//create new user
 
 app.post('/createUser', function(req, res) {
     var newUser = new User({
-        userGoogleToken: '564564545623',
+        userGoogleToken: 'surbhi112.poswalia@gmail.com',
         questionOrder: questionFactory(),
-        results: []
+        results: [0]
     });
     newUser.save(function(err, user) {
         if (err) return errorHandler(res);
@@ -79,29 +58,56 @@ app.post('/createUser', function(req, res) {
     });
 });
 
-app.post('/app/v1/question', function(req, res){
+//get first question displayed, everytime user logs in
+app.get('/question/:currentUserId', function(req, res){
+    var currentUserId = req.params.currentUserId; 
     
-
-      var user_ID = '58211f787b94a534119bb350';    //_id
-       
-    var questionId = '5821184c6c359132fe18f6ba';
-    Questions.findOne({_id:questionId}, function(err, questionJSON){
-        if (err) return errorHandler(res);
-        // return res.json(questionJSON);
-      return updateQuestionOrder(questionJSON);
+     User.findOne({_id:currentUserId}, function(err, user){
+         if (err) return errorHandler(res);
+         console.log(user);
+        return getQuestion(user.questionOrder);
     });
-    
-    function updateQuestionOrder(questionJSON){
-        User.findByIdAndUpdate(user_ID,{
-            questionOrder: [{questionId:'5820fb638ba76026b9bf8b8d',weight:1},{questionId:'5820fdc8b1fa4826e6d8b8b8',weight:1},{questionId:'5820fe1395cf5026fe3dbfca',weight:1}],
-            result: [20,23,43]
-        },function(err, userJSON){
-            if (err) return errorHandler(res);
-            return res.json(questionJSON);
-        });
+    function getQuestion(questionOrder){
+    var questionId = questionOrder[0].questionId;
+    Questions.findOne({_id:questionId}, function(err, questionObject){
+        if (err) return errorHandler(res);
+        return res.json(questionObject);
+    });
     }
-   
-   
+});
+
+//sorting the question's array and responding with the next question
+app.post('/app/v1/question', function(req, res){
+    var answerFlag = req.body.answerFlag;    //current_answer_state from asnc  functions
+    var user_ID = req.body.currentUserId;   //_id(current_user)
+    
+    User.findOne({_id:user_ID}, function(err, userObject){
+          if (err) return errorHandler(res);
+          console.log(userObject);
+          return getCurrentUser(userObject);
+    });
+    function getCurrentUser(currentUser){
+        
+        var currentResult =  currentUser.results.slice(-1),
+            newQuestionOrder = sortQuestion(currentUser.questionOrder, answerFlag),
+            questionId = newQuestionOrder[0].questionId,
+            result = ((answerFlag === 'correct')?(currentResult+10):currentResult);
+        
+        Questions.findOne({_id:questionId}, function(err, questionJSON){
+            if (err) return errorHandler(res);
+            return updateQuestionOrder(questionJSON, newQuestionOrder, result);
+        });
+        function updateQuestionOrder(questionJSON, newQuestionOrder, result){
+            User.findByIdAndUpdate(user_ID,{
+                questionOrder: newQuestionOrder,
+                result: result 
+            },function(err, userJSON){
+                if (err) return errorHandler(res);
+                return res.json(questionJSON);
+            });
+        }
+    }
+        
 });
 
 
@@ -111,3 +117,23 @@ function  errorHandler(res){
         message: 'Internal Server Error'
     });
 }
+
+
+// when new user is created- post happens first
+//to get the questions displayed for new user get(/)
+
+
+//get all the questions
+app.get('/questions', function(req, res){
+    Questions.find({}, function(err, question){
+        if (err) return errorHandler(res);
+        return res.json(question);
+    });
+});
+
+app.get('/getUsers', function(req, res){
+    User.find({}, function(err, users){
+        if (err) return errorHandler(res);
+        return res.json(users);
+    });
+});
