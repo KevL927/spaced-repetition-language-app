c925baf96ee67452590d56d2e55fc1d44ef97daa
KL012 <kevl927@gmail.com>
2017-01-17 05:31:20 +0000

    Externalize Store to its own file

--- client/js/index.js
+++ client/js/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
-import reducers from './reducers/reducers';
+import store from './store';
 import routes from './components/routes';
-
-export const store = createStore(reducers, applyMiddleware(thunk));
 
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
