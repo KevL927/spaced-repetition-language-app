24593a0cf892f2f408cda5663a90c1b551d6b52b
KL012 <kevl927@gmail.com>
2017-02-03 00:24:15 +0000

    Refactor by removing unnecessary import and constants. Make each case into string

--- client/js/reducers/reducers.js
+++ client/js/reducers/reducers.js
@@ -1,16 +1,3 @@
-import * as actions from '../actions/actions';
-
-const FETCH_QUESTION_SUCCESS = actions.FETCH_QUESTION_SUCCESS,
-      FETCH_QUESTION_ERROR = actions.FETCH_QUESTION_ERROR,
-      POST_QUESTION_ANSWERED_ERROR = actions.POST_QUESTION_ANSWERED_ERROR,
-      SET_PREV_ANSWER = actions.SET_PREV_ANSWER,
-      CREATE_NEW_USER_SUCCESS = actions.CREATE_NEW_USER_SUCCESS,
-      CREATE_NEW_USER_ERROR = actions.CREATE_NEW_USER_ERROR,
-      SET_CURRENT_USER_INPUT = actions.SET_CURRENT_USER_INPUT,
-      SET_ANSWER_FLAG = actions.SET_ANSWER_FLAG,
-      SET_LAST_ANSWER_FLAG = actions.SET_LAST_ANSWER_FLAG,
-      USER_LOGOUT = actions.USER_LOGOUT;
-
 const initialState = {
       currentQuestion: null,
       currentAnswer: null,
@@ -33,29 +20,29 @@ export default (state, action) => {
    state = state || initialState;  
    
     switch(action.type) {
-        case FETCH_QUESTION_SUCCESS:
+        case 'FETCH_QUESTION_SUCCESS':
             return Object.assign({}, state, {
                 currentQuestion: action.payload.questionObject.question,
                 currentAnswer: action.payload.questionObject.answer,
                 result: action.payload.result
             });
         
-        case FETCH_QUESTION_ERROR:
+        case 'FETCH_QUESTION_ERROR':
             return Object.assign({}, state, {
                 fetchGetQuestionError: action.payload
             });
         
-        case POST_QUESTION_ANSWERED_ERROR:
+        case 'POST_QUESTION_ANSWERED_ERROR':
             return Object.assign({}, state, {
                 postQuestionAnsweredError: action.payload
             });
             
-        case SET_PREV_ANSWER:
+        case 'SET_PREV_ANSWER':
             return Object.assign({}, state, {
                 prevAnswer: action.payload
             });
         
-        case CREATE_NEW_USER_SUCCESS:
+        case 'CREATE_NEW_USER_SUCCESS':
             return Object.assign({}, state, {
                 accessToken: action.accessToken,
                 currentUserId: action.currentUserId,
@@ -63,25 +50,25 @@ export default (state, action) => {
                 isAuthenticated: true
             });
         
-        case CREATE_NEW_USER_ERROR:
+        case 'CREATE_NEW_USER_ERROR':
             return Object.assign({}, state, {
                 createNewUserError: action.payload
             });
             
-        case SET_CURRENT_USER_INPUT:
+        case 'SET_CURRENT_USER_INPUT':
             return Object.assign({}, state, {
                 currentUserInput: action.payload
             });
         
-        case USER_LOGOUT:
+        case 'USER_LOGOUT':
             return Object.assign({}, state, initialState);
             
-        case SET_ANSWER_FLAG:
+        case 'SET_ANSWER_FLAG':
             return Object.assign({}, state, {
                 currentAnswerFlag : action.payload
             });
             
-        case SET_LAST_ANSWER_FLAG:
+        case 'SET_LAST_ANSWER_FLAG':
             return Object.assign({}, state, {
                 lastAnswerFlag : action.payload
             });
